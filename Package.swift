f8b6d0a015d88a20313eededbe15811eca315b76
Jonathan Thornton <github@jonblatho.com>
2020-10-03 19:58:48 -0500

    fix Package.swift

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIUtilities",
+    platforms: [SupportedPlatform.iOS(.v13), SupportedPlatform.macOS(.v11), SupportedPlatform.tvOS(.v13), SupportedPlatform.watchOS(.v6)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUIUtilities",
             targets: ["SwiftUIUtilities"]),
     ],
-    platforms: [SupportedPlatform.iOS(.v13), SupportedPlatform.macOS(.v11), SupportedPlatform.tvOS(.v13), SupportedPlatform.watchOS(.v6)],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
